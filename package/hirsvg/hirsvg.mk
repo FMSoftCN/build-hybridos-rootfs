fd0af6bb93f138a527dd617daaac6bbb169121ec
xueshuming <xueshuming@minigui.org>
2021-02-07 14:43:02 +0800

    1. remote package/librsvg; 2. BR2_PACKAGE_HIRSVG --> BR2_PACKAGE_LIBRSVG

--- package/hirsvg/hirsvg.mk
+++ package/hirsvg/hirsvg.mk
@@ -4,18 +4,23 @@
 #
 ################################################################################
 
-HIRSVG_SITE = https://gitlab.fmsoft.cn/hybridos/hirsvg
-HIRSVG_VERSION = 47d2f4f9fcafb0f64b45ddd711c9e5f752f6fc8e
-HIRSVG_SITE_METHOD = git
-HIRSVG_INSTALL_STAGING = YES
+LIBRSVG_SITE = https://gitlab.fmsoft.cn/hybridos/hirsvg
+LIBRSVG_VERSION = 47d2f4f9fcafb0f64b45ddd711c9e5f752f6fc8e
+LIBRSVG_SITE_METHOD = git
+LIBRSVG_INSTALL_STAGING = YES
 
-HIRSVG_DEPENDENCIES = hicairo libcroco libglib2 libxml2 pango
+LIBRSVG_DEPENDENCIES = hicairo libcroco libglib2 libxml2 pango
 
 define MINIGUI_RUN_AUTOGEN
     cd $(@D) && PATH=$(BR_PATH) ./autogen.sh "test"
 endef
-HIRSVG_PRE_CONFIGURE_HOOKS += MINIGUI_RUN_AUTOGEN
+LIBRSVG_PRE_CONFIGURE_HOOKS += MINIGUI_RUN_AUTOGEN
 
-HIRSVG_CONF_OPTS = --disable-introspection --disable-pixbuf-loader --enable-minigui-backend
+LIBRSVG_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
+LIBRSVG_CONF_OPTS = --disable-pixbuf-loader --disable-tools --disable-introspection --enable-minigui-backend
+
+HOST_LIBRSVG_CONF_OPTS = --disable-introspection --enable-minigui-backend=no
+HOST_LIBRSVG_DEPENDENCIES = host-cairo host-gdk-pixbuf host-libcroco host-libglib2 host-libxml2 host-pango
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
