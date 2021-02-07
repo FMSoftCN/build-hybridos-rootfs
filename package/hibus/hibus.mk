dedac33c8199ed3fb8bc67a3aa7e7e1bb0c93c91
xueshuming <xueshuming@minigui.org>
2021-02-07 15:38:57 +0800

    hibus BUILD_CMDLINE=OFF

--- package/hibus/hibus.mk
+++ package/hibus/hibus.mk
@@ -1,7 +1,8 @@
 HIBUS_SITE = https://gitlab.fmsoft.cn/hybridos/hibus
-HIBUS_VERSION = 65f90eca6faf3559458ee8a4259f8e8a64144ae6
+HIBUS_VERSION = 99c2e996fa343059e6fe69f57123995878e4a785
 HIBUS_SITE_METHOD = git
 HIBUS_INSTALL_STAGING = YES
 HIBUS_DEPENDENCIES = hibox libopenssl libglib2
+HIBUS_CONF_OPTS = -DBUILD_CMDLINE=OFF
 
 $(eval $(cmake-package))
