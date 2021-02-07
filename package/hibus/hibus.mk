14a9eed0ab8aab3faece7f25012861a2a1d2a9c7
xueshuming <xueshuming@minigui.org>
2021-02-07 11:31:32 +0800

    update hibus

--- package/hibus/hibus.mk
+++ package/hibus/hibus.mk
@@ -1,5 +1,5 @@
 HIBUS_SITE = https://gitlab.fmsoft.cn/hybridos/hibus
-HIBUS_VERSION = 1df4d4419af33189d60285291c6948edc3f8b79b
+HIBUS_VERSION = 65f90eca6faf3559458ee8a4259f8e8a64144ae6
 HIBUS_SITE_METHOD = git
 HIBUS_INSTALL_STAGING = YES
 HIBUS_DEPENDENCIES = hibox libopenssl libglib2
