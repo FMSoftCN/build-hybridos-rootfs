247033b1c8a0b329dad4c236b0658535c54a04ae
xueshuming <xueshuming@minigui.org>
2021-02-08 11:04:36 +0800

    update inetd

--- package/inetd/inetd.mk
+++ package/inetd/inetd.mk
@@ -1,5 +1,5 @@
 INETD_SITE = https://gitlab.fmsoft.cn/hybridos/cn.fmsoft.hybridos.settings
-INETD_VERSION = 820c42f4ef643cd200667d84e739c23445ecb08b
+INETD_VERSION = 3a5bf123095fced6b4c8015ea3f79f41d38ffb16
 INETD_SITE_METHOD = git
 INETD_INSTALL_STAGING = YES
 INETD_DEPENDENCIES = hibox hibus
