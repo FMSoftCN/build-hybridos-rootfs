004c67b62db4ba6c9bfa03ce5cdaa1f746d54b48
xueshuming <xueshuming@minigui.org>
2021-06-02 19:51:56 +0800

    1. add glibc 2.25; 2. add binutil 2.28, 2.29

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,6 +7,11 @@
 ifeq ($(BR2_csky),y)
 GLIBC_VERSION = 7630ed2fa60caea98f500e4a7a51b88f9bf1e176
 GLIBC_SITE = $(call github,c-sky,glibc,$(GLIBC_VERSION))
+else ifeq ($(BR2_PACKAGE_GLIBC_2_25),y)
+GLIBC_VERSION = 2.25
+GLIBC_SITE = $(BR2_GNU_MIRROR)/libc
+GLIBC_SOURCE = glibc-$(GLIBC_VERSION).tar.xz
+GLIBC_SRC_SUBDIR = .
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
